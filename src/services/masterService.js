490a163bda2de8e7bee4105e871bfdcfddcbfa46
Hayat-Husain <husainhayat13@gmail.com>
2025-12-30 13:07:00 +0530

    ok

--- src/services/masterService.js
+++ src/services/masterService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.withCredentials=true;
+//axios.defaults.withCredentials=true;
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 const token=localStorage.getItem("token");
